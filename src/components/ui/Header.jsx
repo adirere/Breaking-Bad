69a6facdab68011ee2ce085c5dab4fc8dbc4cd47
Adrian Bratu <46399102+adirere@users.noreply.github.com>
2020-09-14 22:07:16 +0300

    updated readme

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import logo from "../../images/logo.png";
+import { ReactComponent as Icon } from "../../images/tubeIcon.svg";
 
 const Header = () => {
   return (
     <header className="center">
+      <Icon style={{ position: "relative", width: "50px" }} />
       <img src={logo} alt="Breaking Bad logo" />
     </header>
   );
--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,8 +1,24 @@
 import React, { useState } from "react";
-import logo from "../../images/logo.png";
+import { Input, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  search: {
+    border: "1px solid black",
+    borderRadius: "10px 10px 0 0",
+    backgroundColor: "rgba(255, 255, 255, 0.15);",
+    padding: "10px",
+    margin: "1.5rem auto",
+    display: "flex",
+    flexDirection: "column",
+    color: "white",
+    width: "50%",
+    minWidth: "300px"
+  }
+});
 
 const Search = ({ getQuery }) => {
   const [text, setText] = useState("");
+  const classes = useStyles();
 
   const onChange = e => {
     setText(e);
@@ -10,18 +26,13 @@ const Search = ({ getQuery }) => {
   };
 
   return (
-    <section className="search">
-      <form>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Search characters"
-          value={text}
-          onChange={e => onChange(e.target.value)}
-          autoFocus
-        />
-      </form>
-    </section>
+    <Input
+      color="secondary"
+      className={classes.search}
+      placeholder="Search characters ..."
+      onChange={e => onChange(e.target.value)}
+      value={text}
+    />
   );
 };
 
